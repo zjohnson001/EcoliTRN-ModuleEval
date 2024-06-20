b07f669a790cbae6f356bd02d5cfa19d7f1df58d
Zach Johnson <ZacharyDJohnson.998@gmail.com>
2024-06-20 11:38:08 -0700

    first attempt at updating setup files

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from distutils.core import setup
+from setuptools import setup, Extension
 from Cython.Build import cythonize
 import numpy
 
@@ -7,11 +7,42 @@ import numpy
 cython_files = [
     os.path.join('src', 'modeval', 'ebcubed.pyx'),
     os.path.join('src', 'modeval', 'jaccard.pyx'),
-    os.path.join('src', 'modeval', 'cfisher.pyx')
+]
+
+# Create Extension objects
+extensions = [
+    Extension(
+        name="modeval.ebcubed",
+        sources=[cython_files[0]],
+        include_dirs=[numpy.get_include()],
+    ),
+    Extension(
+        name="modeval.jaccard",
+        sources=[cython_files[1]],
+        include_dirs=[numpy.get_include()],
+    ),
 ]
 
 setup(
     name='cython_modules',
-    ext_modules=cythonize(cython_files),
-    include_dirs=[numpy.get_include()]
+    ext_modules=cythonize(
+        extensions,
+        compiler_directives={'language_level': "3"},
+    ),
+    include_dirs=[numpy.get_include()],
+    install_requires=[
+        'cython>=3.0.10',
+        'numpy>=1.26.4',
+        'fisher>=0.1.14',
+        'munkres>=1.1.4',
+        'pandas>=2.2.2',
+        'statsmodels>=0.14.2',
+        'scipy>=1.13.1',
+        'scikit-learn>=1.5.0',
+        'nbconvert>=7.16.4',
+        'nbformat>=5.10.4',
+        'ipykernel>=6.29.4',
+        'matplotlib>=3.9.0',
+        'networkx>=3.2.1'
+    ]
 )
